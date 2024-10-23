8c9e674a032e8fd061a6701d47d58f1ee90465ec
HNam <90977945+hoangfnam03@users.noreply.github.com>
2024-10-23 09:43:55 +0700

    Update ServerHome.java
    update

--- Server-Client-Monitoring-System/Netbeans Project/Server_Client_Monitoring_System/src/ServerHome.java
+++ Server-Client-Monitoring-System/Netbeans Project/Server_Client_Monitoring_System/src/ServerHome.java
@@ -10,16 +10,6 @@ import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Ankur
- */
 public class ServerHome extends javax.swing.JFrame {
 
     /**
