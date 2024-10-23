b7df51d6f318b9e54be4c541512049871d497f6d
HNam <90977945+hoangfnam03@users.noreply.github.com>
2024-10-23 09:42:30 +0700

    Update ClientHome.java
    update

--- Server-Client-Monitoring-System/Netbeans Project/Server_Client_Monitoring_System/src/ClientHome.java
+++ Server-Client-Monitoring-System/Netbeans Project/Server_Client_Monitoring_System/src/ClientHome.java
@@ -4,18 +4,6 @@ import java.awt.Font;
 import javax.swing.UIManager;
 import javax.swing.plaf.ColorUIResource;
 
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Ankur
- */
 public class ClientHome extends javax.swing.JFrame {
 
     /**
